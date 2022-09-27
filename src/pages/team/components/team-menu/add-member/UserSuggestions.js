12f09720c5f4ffb7d56bc323e97339685605d9cb
juelhossain <juelhossainnn@gmail.com>
2022-09-27 11:46:24 +0600

    suggestion scrollbar added

--- src/pages/team/components/team-menu/add-member/UserSuggestions.js
+++ src/pages/team/components/team-menu/add-member/UserSuggestions.js
@@ -1,3 +1,4 @@
+import ScrollBar from "react-perfect-scrollbar";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../../../../features/auth/authSelector";
 import { isUserExist } from "../../../../../utils/lib";
@@ -17,39 +18,41 @@ const UserSuggestions = ({
     showSuggestion && (
       <div
         style={{ borderColor: color["200"] }}
-        className={`p-2 flex flex-col gap-2 max-h-28 overflow-auto absolute w-full bg-white shadow-md border rounded mt-1 z-10`}
+        className={`p-2   absolute w-full bg-white shadow-md border rounded mt-1 z-10`}
       >
-        {users?.map((user) => {
-          if (user.email !== myEmail) {
-            return (
-              <div
-                onClick={() => {
-                  setInput(user.email);
-                  setUserToAdd(user.email);
-                  setShowSuggestion(false);
-                }}
-                key={user.id}
-                className={`py-1 px-3 rounded px-3cursor-pointer justify-between flex items-center border shadow-sm cursor-pointer`}
-                style={{ ...color.common, borderColor: color?.["200"] }}
-              >
-                <p> {user?.email}</p>
-                {isUserExist(members, user.email) && (
-                  <p
-                    className={`text-[10px] rounded px-1 -mr-2 `}
-                    style={{
-                      backgroundColor: color?.["900"],
-                      color: color?.["50"],
-                    }}
-                  >
-                    Member
-                  </p>
-                )}
-              </div>
-            );
-          } else {
-            return null;
-          }
-        })}
+        <ScrollBar className="flex flex-col gap-2 max-h-28 ">
+          {users?.map((user) => {
+            if (user.email !== myEmail) {
+              return (
+                <div
+                  onClick={() => {
+                    setInput(user.email);
+                    setUserToAdd(user.email);
+                    setShowSuggestion(false);
+                  }}
+                  key={user.id}
+                  className={`py-1 px-3 rounded px-3cursor-pointer justify-between flex items-center border shadow-sm cursor-pointer`}
+                  style={{ ...color.common, borderColor: color?.["200"] }}
+                >
+                  <p> {user?.email}</p>
+                  {isUserExist(members, user.email) && (
+                    <p
+                      className={`text-[10px] rounded px-1 -mr-2 `}
+                      style={{
+                        backgroundColor: color?.["900"],
+                        color: color?.["50"],
+                      }}
+                    >
+                      Member
+                    </p>
+                  )}
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </ScrollBar>
       </div>
     )
   );
